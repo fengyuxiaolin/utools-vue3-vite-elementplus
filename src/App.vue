ca6c319dc9aa61d3c9e0c46a5d653757a7dd12e5
fengyuxiaolin <tangminxiang123>
2022-09-22 17:15:20 +0800

    基础demo

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,24 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+// 数据库增删查 demo
+proxy.$api.postApi({
+  prefix: 'hello_doc',
+  data: {
+    str: 'hello'
+  }
+})
+proxy.$api.allDocsApi('hello_doc').then((allDocs) => {
+  console.log('allDocs: ', allDocs);
+  allDocs.forEach(doc => {
+    proxy.$api.removeApi(doc)
+  });
+})
 </script>
 
 <template>
-  <HelloWorld/>
+  <HelloWorld />
 </template>
 
 <style>
